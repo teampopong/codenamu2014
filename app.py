88f3bb0384b3a1ee82b20e9baab1792f983168a1
Lucy Park <me@lucypark.kr>
2014-10-25 13:15:46 +0900

    Add main page

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ app = Flask(__name__)
 from home import get_names
 
 @app.route("/")
-def hello():
+def home():
+    return render_template('home.html', id_names=get_names()[:10])
+
+@app.route("/<member_num>")
+def hello(member_num):
 	f = open('19th members.json', 'r')
 	datas = json.load(f)
-	data = datas[0]
+	data = datas[int(member_num)]
 	data['yea'] = len([vote for vote in data['votes'] if vote['option'] == 'yea'])
 	data['nay'] = len([vote for vote in data['votes'] if vote['option'] == 'nay'])
 	data['forfeit'] = len([vote for vote in data['votes'] if vote['option'] == 'forfeit'])
---
+++ home.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import json
+
+def get_names():
+    with open('19th members.json', 'r') as f:
+        data = json.load(f)
+    return [(i, d.get('name')) for i, d in enumerate(data)]
